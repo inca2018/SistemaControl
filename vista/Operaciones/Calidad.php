b29c31ebf4a75dd0acc6144c92743d8ac066290d
Jesus Inca <jic_d12@hotmail.com>
2018-11-08 17:24:27 -0500

    wef

--- vista/Operaciones/Calidad.php
+++ vista/Operaciones/Calidad.php
@@ -5,23 +5,55 @@
 <?php require_once('../layaout/Nav.php');?>
 <!-- Fin  del Cuerpo y Nav -->
 <!-- Cuerpo del sistema de Menu -->
-   <!-- Main section-->
-   <section class="section-container">
-      <!-- Page content-->
-      	<div class="content-wrapper">
-            <div class="content-heading">
-              <div>Gestión de Calidad</div>
-            </div>
-            <!-- START card-->
-            <div class="card card-default">
-               <div class="card-body">
+<!-- Main section-->
+<section class="section-container">
+	<!-- Page content-->
+	<div class="content-wrapper">
 
+		<!-- START card-->
+		<div class="card card-default">
+			<div class="card-body">
+				<div class="card-body">
+					<div class="row ">
+						<div class="col-md-12 w-100 text-center ">
+							<h3>Gestión de Calidad:</h3>
+						</div>
+					</div>
+					<h5 class="mt-3 mb-3 titulo_area"><em><b>Lista de Ovillos Recepcionados para Calidad:</b></em></h5>
+					<div class="row ">
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-12">
+									<table class="table w-100 table-hover table-sm dt-responsive nowrap" id="tablaPersona">
+										<thead class="thead-light text-center">
+											<tr>
+												<th data-priority="1">#</th>
+												<th>ESTADO</th>
+												<th>Nº ORDEN</th>
+												<th>MATERIAL</th>
+												<th>LOTE</th>
+												<th>KILOS</th>
+												<th>TRABAJADOR</th>
+												<th>Nª DE CONO</th>
+												<th>ACCIONES</th>
+											</tr>
+										</thead>
+										<tbody>
+										</tbody>
+									</table>
+								</div>
 
-               </div>
-           </div>
-         </div>
-   </section>
-    <!-- Fin Modal Agregar-->
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Fin Modal Agregar-->
 <!-- Fin del Cuerpo del Sistema del Menu-->
 <!-- Inicio del footer -->
 <?php require_once('../layaout/Footer.php');?>
